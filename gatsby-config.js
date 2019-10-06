07e44ca056b9b3913cb90092b75a9580f07ae153
mbeche <mb.johnsonbece@gmail.com>
2019-10-06 13:52:02 -0500

    Commit 2: Update homepage, rename page-2 to blogposts, and update siteMetadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `My Awesome Blog`,
+    description: `An awesome place where all my posts live`,
+    author: `@micLeners`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
